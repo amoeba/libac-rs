f37a1e4dfc10c3e68e50e27c59beb3de6ee238e6
Bryce Mecum <petridish@gmail.com>
2025-03-16 10:22:46 -0700

    Make main generate more images

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
-use std::{fs::File, io::{BufWriter, Seek}};
+use std::{
+    fs::File,
+    io::{BufWriter, Seek},
+};
 
 use deku::DekuContainerRead;
-use image::{DynamicImage, ImageBuffer, RgbaImage};
 use libac_rs::{dat::file_types::texture::Texture, icon::Icon};
 
-fn main() -> Result<(), std::io::Error>{
-    // Offset: 254331904 (skip first two DWORDS for now)
-    let mut file = File::options().read(true).open("../ACEmulator/ACE/Dats/client_portal.dat").unwrap();
-    file.seek(std::io::SeekFrom::Start(254331904+8))?; // (skip first two DWORDS for now)
+fn main() -> Result<(), std::io::Error> {
+    let offsets = [254331904, 184571904, 885193728];
+    let mut file = File::options()
+        .read(true)
+        .open("../ACEmulator/ACE/Dats/client_portal.dat")
+        .unwrap();
+    for offset in offsets {
+        file.seek(std::io::SeekFrom::Start(offset + 8))?; // (skip first two DWORDS for now)
 
-    let (_, texture) = Texture::from_reader((&mut file, 0))?;
-    texture.to_png("test.png", 1)?;
+        let (_, texture) = Texture::from_reader((&mut file, 0))?;
 
-    // Offset: 184571904
-    file.seek(std::io::SeekFrom::Start(184592384+8))?;
-    let (_, overlay_texture) = Texture::from_reader((&mut file, 0))?;
-    overlay_texture.to_png("overlay.png", 1)?;
+        // Offset: 184571904
+        // file.seek(std::io::SeekFrom::Start(184592384+8))?;
+        // let (_, overlay_texture) = Texture::from_reader((&mut file, 0))?;
+        // overlay_texture.to_png("overlay.png", 1)?;
 
-    // icon now?
-    let icon = Icon {
-        width: 32,
-        height: 32,
-        scale: 4,
-        base: texture,
-        underlay: Some(overlay_texture),
-        overlay: None,
-        overlay2: None,
-        effect: None,
-    };
+        // icon now?
+        let icon = Icon {
+            width: 32,
+            height: 32,
+            scale: 8,
+            base: texture,
+            underlay: None,
+            overlay: None,
+            overlay2: None,
+            effect: None,
+        };
 
-    icon.export_to_file("icon.png")?;
+        icon.export_to_file(&format!("{}.png", offset))?;
+    }
 
     Ok(())
 }
