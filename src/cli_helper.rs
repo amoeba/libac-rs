5a79ae0caa45ec4bb4934790fbe5e246710ab649
Bryce Mecum <petridish@gmail.com>
2025-06-06 20:53:35 -0700

    Very shitty but working CLI code

--- src/cli_helper.rs
+++ src/cli_helper.rs
@@ -5,15 +5,47 @@ use std::{
     io::{Cursor, SeekFrom},
 };
 
+use libac_rs::dat::reader::dat_directory_entry::DatDirectoryEntry;
 use libac_rs::dat::{
     enums::dat_file_type::DatFileType,
     file_types::{dat_file::DatFile, texture::Texture},
     reader::{dat_block_reader::DatBlockReader, dat_database::DatDatabase},
 };
 
-pub async fn extract_texture_by_id(
+pub async fn index_dat(dat_file_path: &str) -> Result<DatDatabase, Box<dyn Error>> {
+    let mut db_file = File::open(dat_file_path)?;
+    let db = DatDatabase::read(&mut db_file)?;
+
+    Ok(db)
+}
+
+pub async fn find_file_by_id(
+    db: &DatDatabase,
     object_id: &str,
+) -> Result<DatDirectoryEntry, Box<dyn Error>> {
+    // TODO: Factor out into testable helper
+    // Convert hex string to u32
+    let parsed_id = if object_id.starts_with("0x") {
+        u32::from_str_radix(&object_id[2..], 16)?
+    } else {
+        u32::from_str_radix(object_id, 16)?
+    };
+
+    println!("parsed_id: {}", parsed_id);
+    let files = db.list_files(true)?;
+    let target_file = files.iter().find(|file| file.object_id == parsed_id);
+
+    match target_file {
+        Some(file) => Ok(*file),
+        None => {
+            return Err(format!("Object ID {} not found in DAT file", object_id).into());
+        }
+    }
+}
+
+pub async fn extract_texture_by_id(
     dat_file_path: &str,
+    object_id: &str,
     output_dir: &str,
 ) -> Result<(), Box<dyn Error>> {
     // Convert hex string to u32
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 pub mod cli_helper;
 
-use std::error::Error;
+use std::{error::Error, io::Cursor, path::Path, ptr::read};
 
 use clap::{Parser, Subcommand};
+use libac_rs::dat::{
+    file_types::{
+        dat_file::{self, DatFile, DatFileRead},
+        texture::{self, Texture},
+    },
+    reader::async_file_reader::{DatFileReader, FileRangeReader},
+};
+use tokio::fs::File;
 
-use crate::cli_helper::extract_texture_by_id;
+use crate::cli_helper::{extract_texture_by_id, find_file_by_id, index_dat};
 
 #[derive(Parser)]
 #[command(name = "dat")]
@@ -17,8 +25,12 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Extract {
-        #[arg(help = "Path to DAT file (e.g., ./portal_1.dat)", short, long)]
-        dat_path: String,
+        #[arg(
+            help = "Path to DAT file (e.g., ./client_portal.dat)",
+            short('f'),
+            long("dat_file")
+        )]
+        dat_file: String,
         #[arg(help = "Object ID to extract (e.g., 0321)")]
         object_id: String,
         #[arg(short, long, default_value = "./")]
@@ -32,14 +44,48 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     match cli.command {
         Commands::Extract {
-            dat_path,
+            dat_file,
             object_id,
             output_dir,
         } => {
             println!(
                 "Extract: {:?}, {:?}, {:?}!",
-                dat_path, object_id, output_dir
+                dat_file, object_id, output_dir
             );
+
+            let dat = index_dat(&dat_file).await?;
+            let found_file = find_file_by_id(&dat, &object_id).await?;
+
+            println!("Found file: {:?}", dat_file);
+
+            match found_file.file_type() {
+                libac_rs::dat::enums::dat_file_type::DatFileType::Texture => {
+                    let file = tokio::fs::File::open(&dat_file).await?;
+                    let compat_file = tokio_util::compat::TokioAsyncReadCompatExt::compat(file);
+                    let mut file_reader = FileRangeReader::new(compat_file);
+                    let mut reader = DatFileReader::new(
+                        found_file.file_size as usize,
+                        dat.header.block_size as usize,
+                    )?;
+                    let result = reader
+                        .read_file(&mut file_reader, found_file.file_offset)
+                        .await
+                        .unwrap();
+
+                    let mut y = Cursor::new(result.buffer);
+                    let outer_file: DatFile<Texture> = DatFile::read(&mut y)?;
+                    let texture = outer_file.inner;
+                    let output_path = format!("{}.png", object_id);
+                    texture.to_png(&output_path, 1)?;
+                    println!("Texture saved to {:?}", output_path);
+                }
+                _ => {
+                    println!(
+                        "Unsupported file type for extraction: {:?}",
+                        found_file.file_type()
+                    );
+                }
+            }
         }
     }
 
